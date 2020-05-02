1b3e36af14f3d8e68cd53dabefb9416a25338481
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-05-02 05:36:13 +0200

    added a filter method

--- src/main/java/io/amin/blog/repositories/CommentRepository.java
+++ src/main/java/io/amin/blog/repositories/CommentRepository.java
@@ -1,5 +1,7 @@
 package io.amin.blog.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,8 @@ import io.amin.blog.models.Comment;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
     
+    List<Comment> findByBlogPostId(int blogPostId);
+
+    List<Comment> findByAuthorId(int authorId);
+
 }

c9076de15f40d0ced7257cabd4cf23aae7383b39
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-05-02 06:09:10 +0200

    refactored package name

--- src/test/java/io/amin/blog/BlogApplicationTests.java
+++ src/test/java/io/amin/blog/BlogApplicationTests.java
@@ -1,10 +1,10 @@
-package io.amin.personalblog;
+package io.amin.blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BlogApplicationTests {
 
 	@Test
 	void contextLoads() {

234a1a2c6abd53a06ec7ad95e6f8b6c4c7f7ae8e
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-05-02 07:56:51 +0200

    implemented tag service functionality

--- src/main/java/io/amin/blog/services/TagService.java
+++ src/main/java/io/amin/blog/services/TagService.java
@@ -1,8 +1,11 @@
 package io.amin.blog.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import io.amin.blog.models.Tag;
 import io.amin.blog.repositories.TagRepository;
 
 @Service
@@ -10,5 +13,30 @@ public class TagService {
     
     @Autowired
     private TagRepository tagRepository;
+
+	public List<Tag> getAllTags() {
+		return tagRepository.findAll();
+	}
+
+	public Tag getTagById(int tagId) {
+		return tagRepository.findById(tagId).orElseThrow();
+	}
+
+	public Tag createNewTag(Tag tag) {
+		return tagRepository.save(tag);
+	}
+
+	public Tag updateTagDetails(int tagId, Tag tag) {
+        if(tag.getId() != tagId) {
+            // TODO throw an error
+        }
+		return tagRepository.save(tag);
+	}
+
+	public Tag deleteTagById(int tagId) {
+        Tag tag = tagRepository.findById(tagId).orElseThrow();
+        tagRepository.deleteById(tagId);
+		return tag;
+	}
 
 }

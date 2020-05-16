b38e1b914c0cf33406782204f7ae82392621b5d1
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-05-16 15:36:30 +0200

    removed unneeded failing empty test

--- src/test/java/io/amin/blog/BlogApplicationTests.java
+++ src/test/java/io/amin/blog/BlogApplicationTests.java
@@ -1,13 +1,8 @@
 package io.amin.blog;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlogApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }

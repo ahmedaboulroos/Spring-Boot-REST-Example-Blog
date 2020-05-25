856db6279dfa2ed8f3a464f42f95b803d478f49e
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-05-25 23:38:13 +0200

    added find by name method

--- src/main/java/io/amin/blog/repositories/TagRepository.java
+++ src/main/java/io/amin/blog/repositories/TagRepository.java
@@ -1,11 +1,12 @@
 package io.amin.blog.repositories;
 
+import io.amin.blog.models.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import io.amin.blog.models.Tag;
-
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Integer> {
-    
+
+    Tag findByName(String name);
+
 }

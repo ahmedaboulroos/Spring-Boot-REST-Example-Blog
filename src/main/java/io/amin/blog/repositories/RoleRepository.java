ecdf760b802ae52fa18d6ea8194692fc531c9079
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-05-25 19:33:36 +0200

    added the missing repository annotation

--- src/main/java/io/amin/blog/repositories/RoleRepository.java
+++ src/main/java/io/amin/blog/repositories/RoleRepository.java
@@ -1,9 +1,10 @@
 package io.amin.blog.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import io.amin.blog.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
-    
+
 }
